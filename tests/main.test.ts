6703471bdb314b5ef1c4043443b80f78b04a83aa
Max Isom <hi@maxisom.me>
2023-02-21 13:58:09 -0800

    Fix type when using a RouteDef object with multiple methods

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,7 @@
 import test from "ava"
 import { expectTypeOf } from "expect-type"
 import { MatchingRoute, TypedAxios } from "../src"
-import { ExampleRouteTypes1 } from "./example-route-types"
+import { ExampleRouteTypes1, ExampleRouteTypes3 } from "./example-route-types"
 
 test("TypedAxios should create nicely typed AxiosInstance", async (t) => {
   const axios: TypedAxios<ExampleRouteTypes1> = null as any
@@ -25,4 +25,21 @@ test("TypedAxios should create nicely typed AxiosInstance", async (t) => {
 
   // @ts-expect-error
   axios.post("/things/create", {})
+})
+
+test("works with RouteDef object that has multiple methods", async (t) => {
+  const axios: TypedAxios<ExampleRouteTypes3> = null as any
+  const getRes = await axios.get("/things/get", {
+    params: {
+      thing_id: "123",
+    },
+  })
+
+  expectTypeOf(getRes.data).toMatchTypeOf<{
+    thing: {
+      thing_id: string
+      name: string
+      created_at: string
+    }
+  }>()
 })
